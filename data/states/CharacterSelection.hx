ffc41a3eb0d03abfdd55d0c5b45774296e95144b
SorbetLover <umc4r4aa3@gmail.com>
2024-06-08 01:56:43 -0300

    bruhh

--- data/states/CharacterSelection.hx
+++ data/states/CharacterSelection.hx
@@ -1,3 +1,8 @@
-function create(){
-	FlxG.sound.music.destroy();
+// import funkin.editors.ui.UIModState;
+import funkin.editors.ui.UIState;
+
+function update(){
+	if(FlxG.keys.justPressed.P){
+		FlxG.switchState(new UIState(true,"CharacterBruteForceAlt"));
+	}
 }
