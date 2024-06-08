7ed5ba130f2894cc7886d47a553affcbbc621016
SorbetLover <umc4r4aa3@gmail.com>
2024-06-08 02:17:46 -0300

    uh

--- data/states/CharacterBruteForceAlt.hx
+++ data/states/CharacterBruteForceAlt.hx
@@ -5,6 +5,8 @@ import funkin.editors.ui.UIState;
 import funkin.editors.ui.UIButton;
 import haxe.io.Path;
 
+import sys.FileSystem;
+
 import flixel.group.FlxSpriteGroup;
 // var window:UIWindow;
 var txtinput:UITextBox;
@@ -18,7 +20,17 @@ var cnt:Int = -1;
 var lstcnt:Inst = -1;
 var col:Int = 0;
 // var files:Array = [];
+var guh:Array = [];
 function postCreate(){
+	for(e in FileSystem.readDirectory("mods/alloyshit/data/characters")){
+	    if(FileSystem.isDirectory("mods/alloyshit/data/characters/" + e)){
+	        trace(Path.withoutExtension(e));
+        	guh.push(e);
+
+	    }
+    }
+	trace(guh);
+
 	btG = new FlxSpriteGroup();
     window = new UIWindow(0,0,1280,720, "Char Brute Load");
 	add(window);
@@ -27,10 +39,10 @@ function postCreate(){
     // txtinput.screenCenter();
 	
 	// add(txtinput);
-    for(i in foldersToCheck){
+    for(i in guh){
         lstcnt += 1;
         cnt = -1;
-        var files = Paths.getFolderContent("data/characters/" + i + "/");
+        var files = Paths.getFolderContent("data/characters/" + guh[cnt + 1] + "/");
 
         for(e in files){
             cnt += 1;
@@ -39,8 +51,8 @@ function postCreate(){
 		    }
             var uhhbt = new UIButton(20 + (230 * lstcnt), 40 + (40 * cnt), files[cnt], function(){FlxG.switchState(new CharacterEditor(i + "/" + Path.withoutExtension(e)));}, 200, 30);
 		    add(uhhbt);
-	    	trace(cnt);
-	    	trace(Path.withoutExtension(e));
+	    	// trace(cnt);
+	    	// trace(Path.withoutExtension(e));
             btG.add(uhhbt);
         }
     }   	
