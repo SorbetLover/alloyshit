29fcb10485b5bbd6a165e3a348f99bcd8cb13209
umc4r4aa3 <umc4r4aa3@gmail.com>
2025-10-30 19:27:23 -0300

    funy

---
+++ songs/demonila/scripts/test.hx
@@ -0,0 +1,15 @@
+// // function postCreate() {
+// // 	for(i in 0...3){
+// // 		strumLines.members[1].members[i].scrollSpeed = FlxG.random.float(1,3);
+// // 		strumLines.members[0].members[i].scrollSpeed = FlxG.random.float(1,3);
+// // 	}
+// // }
+
+// var delta = 0;
+// function update(elapsed) {
+// 	delta += elapsed;
+// 	for(i in [0,1,2,3]){
+// 		strumLines.members[1].members[i].scrollSpeed = (20 + 10 * Math.sin(delta - (i/2))) / 10;
+// 	}
+// }
+/// funny speed changing
