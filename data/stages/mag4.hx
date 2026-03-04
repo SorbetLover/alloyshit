dc9aea77b4b615974ec13aefc8a3cac5bb25b0ee
Anthony <89319609+SorbetLover@users.noreply.github.com>
2026-03-04 08:04:47 -0300

    Update mag4.hx

--- data/stages/mag4.hx
+++ data/stages/mag4.hx
@@ -1,24 +1,25 @@
 var thrw:Bool = false;
 function update(elapsed){
+		player.cpu = PlayState.chartingMode;
 if(thrw == false){
 	switch(strumLines.members[2].characters[0].getAnimName()){
 		case "singLEFT":
-			mcfunny.x = 100 - FlxG.random.int(0, 20);
+			mcfunny.x = 100 - FlxG.random.int(0, 40);
 			mcfunny.y = 74;
 
 			mcfunny.angle = -15;
 		case "singRIGHT":
-			mcfunny.x = 290  + FlxG.random.int(0,20);
+			mcfunny.x = 290  + FlxG.random.int(0,40);
 			
 			mcfunny.angle = 15;
 			mcfunny.y = 74;
 		case "singUP":
-			mcfunny.y = 190  + FlxG.random.int(0,20);
+			mcfunny.y = 190  + FlxG.random.int(0,40);
 			mcfunny.x = 191;
 
 			mcfunny.angle = 0;
 		case "singDOWN":
-			mcfunny.y = -10  - FlxG.random.int(0,20);
+			mcfunny.y = -10  - FlxG.random.int(0,40);
 			mcfunny.x = 191;
 			
 			mcfunny.angle = 0;
